4e624350fc8d1bdb58284de2001a295e9571fd65
Kyle M Hall <kyle@bywatersolutions.com>
2022-04-14 14:35:02 -0400

    BYWATER SOLUTIONS - Customizations for ByWater Solutions

--- website/src/shared/Features.tsx
+++ website/src/shared/Features.tsx
@@ -1,15 +1,5 @@
-import {
-  faBomb,
-  faCodeBranch,
-  faDownload,
-  faLock,
-  faShareAlt,
-  faUserAltSlash,
-  IconDefinition,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
-import { Grid, Paper, Typography, Divider, Box } from '@mui/material';
+import { Grid, Typography, Divider, Box } from '@mui/material';
 
 export const Features = () => {
   const { t } = useTranslation();
@@ -21,64 +11,9 @@ export const Features = () => {
           <Typography variant="h5" align={'center'}>
             {t('features.title')}
           </Typography>
-          <Typography variant="body2" align={'center'}>
-            {t('features.subtitle')}
-          </Typography>
         </Box>
       </Grid>
-      <Feature title={t('features.featureEndToEndTitle')} icon={faLock}>
-        {t('features.featureEndToEndText')}
-      </Feature>
-      <Feature title={t('features.featureSelfDestructionTitle')} icon={faBomb}>
-        {t('features.featureSelfDestructionText')}
-      </Feature>
-      <Feature title={t('features.featureOneTimeTitle')} icon={faDownload}>
-        {t('features.featureOneTimeText')}
-      </Feature>
-      <Feature
-        title={t('features.featureSimpleSharingTitle')}
-        icon={faShareAlt}
-      >
-        {t('features.featureSimpleSharingText')}
-      </Feature>
-      <Feature
-        title={t('features.featureNoAccountsTitle')}
-        icon={faUserAltSlash}
-      >
-        {t('features.featureNoAccountsText')}
-      </Feature>
-      <Feature title={t('features.featureOpenSourceTitle')} icon={faCodeBranch}>
-        {t('features.featureOpenSourceText')}
-      </Feature>
     </Grid>
   );
 };
 
-type FeatureProps = {
-  readonly title: string;
-  readonly icon: IconDefinition;
-  readonly children: JSX.Element;
-};
-
-const Feature = (props: FeatureProps) => {
-  return (
-    <Grid item={true} xs={12} md={4}>
-      <Paper
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          minHeight: '230px',
-          padding: '16px',
-        }}
-      >
-        <FontAwesomeIcon color={'black'} size={'4x'} icon={props.icon} />
-        <Typography sx={{ padding: '5px' }} variant="h5">
-          {props.title}
-        </Typography>
-        <Typography>{props.children}</Typography>
-      </Paper>
-    </Grid>
-  );
-};
--- website/src/theme.ts
+++ website/src/theme.ts
@@ -1,8 +1,25 @@
 import { createTheme } from '@mui/material/styles';
-import { blueGrey } from '@mui/material/colors';
 
 export const theme = createTheme({
   palette: {
-    primary: blueGrey,
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#1B4B81',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#8CCAED',
+      main: '#6899B9',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ECEDEE',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
   },
 });
